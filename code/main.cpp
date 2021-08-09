19697256e9180117c74e9318a6809111a9f59def
unknown <omaresmael590@gmail.com>
2021-08-09 14:43:54 +0200

    Recursion

--- code/main.cpp
+++ code/main.cpp
@@ -9,7 +9,7 @@ void printArr(int *arr, int size) {
     }
 
 }
-
+//chapter 1
 int binary_search(const int *arr, int val, int size)
 {
     int low = 0;
@@ -19,6 +19,7 @@ int binary_search(const int *arr, int val, int size)
     {
         mid = (low + high)/2;
         if(arr[mid] == val) return mid;
+
         if(arr[mid] > val) high = mid-1;
         else low = mid +1;
 
@@ -26,6 +27,7 @@ int binary_search(const int *arr, int val, int size)
     return -1;
 
 }
+//chapter 2
 void selection_sort(int *arr,int size)
 {
 
@@ -47,18 +49,67 @@ void selection_sort(int *arr,int size)
 
     }
 }
+//chapter 4
+int divide_conquer(int *arr, int size) //return the sum of array using D&C
+{
+    if (size == 0)
+        return 0;
+    size --;
+    return arr[size] + divide_conquer(arr,size);
+}
+
+int divide_conquer_tail_recursion(int *arr, int size, int sum) //This is better in terms of performance
+{
+    if (size == 0)
+        return sum;
+
+    size --;
+    return divide_conquer_tail_recursion(arr,size,arr[size] + sum);
+}
+
+int max_number(int *arr, int size,int max) //return the sum of array using D&C
+{
+    if (size == 0)
+        return max;
+    size --;
+    if (max < arr[size])
+        max = arr[size];
+    return max_number(arr,size,max);
+}
+
+int binary_search_recursion(const int *arr, int val, int size,int low,int high)
+{
+
+    int mid = (low + high)/2;
+    if (arr[mid] == val)
+        return mid;
+    if (low >= high)
+        return -1;
+
+    if(arr[mid] > val)
+        high = mid-1;
+
+    else
+        low = mid +1;
+
+    return binary_search_recursion(arr,val,size,low,high);
+}
 
 
 
 
 int main() {
-    int arr[] = {6,9,15,3};
+    int arr[] = {1,2,3,4,5,6,7,8,9};
     int size = sizeof(arr)/sizeof(arr[0]);
-    int sortedArr[size];
-    selection_sort(arr,size);
-    printArr(arr,size);
+
+//    selection_sort(arr,size);
+//    printArr(arr,size);
+//    cout << divide_conquer(arr,size);
+//    cout << divide_conquer_tail_recursion(arr,size,0);
+//    cout << max_number(arr,size,-1);
 //    cout << binary_search(arr,13,sizeof(arr)/sizeof(arr[0]));
-    return 0;
+    cout << binary_search_recursion(arr,10,size,0,size-1);
+//    return 0;
 }
 
 
